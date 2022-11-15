6bdbc0238da68a690f63413010b81b3d6c3314f8
callor <callor@callor.com>
2022-11-15 15:31:42 +0900

    router_Arrow

--- templates/routes/index.js
+++ templates/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "callor.com Express" });
 });
 
